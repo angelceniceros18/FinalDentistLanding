e2231a45a77010d089dc05a7120acee7503d8056
AngelC <omar.ceniceros.berumen.18@gmail.com>
2020-01-14 11:54:19 -0600

    Intento de colocar estilos de bootstrap y la barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Navigation/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation
   }
 }
 </script>
