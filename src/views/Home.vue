9c8a74005a74d0cc49534eb7b9ee3d18fbe2b342
honeyflake23 <59746084+honeyflake23@users.noreply.github.com>
2020-01-15 10:50:52 -0600

    SCSS
    Adición de scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="app">
         <main>
+            <TopBar/>
             <h1>Hola</h1>
         </main>
     </div>
@@ -8,8 +9,13 @@
 
 <script>
 
+import TopBar from '@/components/Navigation.vue'
+
 export default {
-    
+    name: 'Home',
+    components: {
+        TopBar,
+    }
 }
 </script>
 
