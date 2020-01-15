3cf13d58ecee30bef6f5b8d771ebd165a884161c
AngelC <omar.ceniceros.berumen.18@gmail.com>
2020-01-15 14:00:46 -0600

    fix
    quite prueba del router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,13 @@
         <main>
             <Navigation/>
             <Hero/>
+            <Divider/>
+            <Feature/>
             <Pricing/>
-            
+            <Download/>
+            <Contact/>
             <Footer/>
+            
         </main>
     </div>
 </template>
@@ -17,10 +21,18 @@ import Navigation from '@/components/Navigation.vue'
 import Hero from '@/components/Hero.vue'
 import Footer from '@/components/PageFooter.vue'
 import Pricing from '@/components/Pricing.vue'
+import Contact from '@/components/Contact.vue'
+import Divider from '@/components/Divider.vue'
+import Feature  from '@/components/Feature.vue'
+import Download from '@/components/Download.vue'
 export default {
     name: 'Home',
     components: {
         Navigation,
+        Contact,
+        Divider,
+        Feature,
+        Download,
         Hero,
         Footer,
         Pricing
