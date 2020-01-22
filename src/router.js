1a35d70b8fedcdb96f1daac5aa5fd417a12d526d
AngelC <omar.ceniceros.berumen.18@gmail.com>
2020-01-22 12:12:36 -0600

    error

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Features from './views/Features.vue';
-
 import Comprar from './views/Comprar.vue'
 Vue.use(Router);
 
@@ -13,11 +11,6 @@ const router = new Router({
         path: '/',
         name: 'home',
         component: Home,
-      },
-      {
-        path: '/features',
-        name: 'features',
-        component: Features,
       },
       {
         path:'/Comprar',
